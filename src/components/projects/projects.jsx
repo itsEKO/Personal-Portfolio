acb990c68d87d41cdc300f5e7fb064b6225558d5
itsEKO <91019944+itsEKO@users.noreply.github.com>
2022-03-23 15:47:45 -0700

    Update projects.jsx

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -8,7 +8,7 @@ export default function projects() {
         <h1 className="sect-head">Bitcoin Tracker</h1>
         <p className="info-text"> Bitcoin price tracker using coindesk's API to track and chart the price of bitcoin from the last 30 days along with a UI Mockup</p>
         <div id="btn-cnt">
-          <a id="button" href="https://itseko.github.io" target="_blank" rel="noreferrer">Visit</a>
+          <a id="button" href="https://itseko.github.io/BTC-tracker" target="_blank" rel="noreferrer">Visit</a>
         </div>
       </div>
       <div className="image-cont">
